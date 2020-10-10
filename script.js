cb4c4a6d630d10029cbafc0323f164dd941ce3f6
Violet Mae <rideuforfun@gmail.com>
2020-10-09 21:56:30 -0600

    finshed variables and now adding quiz questions

--- script.js
+++ script.js
@@ -8,3 +8,29 @@ var answerButtons = document.getElementsById('button-answer');
 var questionCounter= 0;
 var userScore = 0;
 var questionNumber = 0;
+var userAnswer ="";
+var count;
+var answerCheck = "";
+var interval;
+var scoreBoard = [];
+constmyQuestions= [
+{
+Question: "Who said Toto, I've got a feeling we're not in Kansas anymore?",
+answers: {
+A: "Donald Trump",
+B: "Dorthy",
+C: "Auntie Em"
+D: "Glenda the Good Witch of the South"
+},
+correctAnswer: "B"
+},
+{
+Question: "Who is Jerry Seinfeld's favorite superhero?",
+answers: {
+    A: "Batman",
+    B: "Posion Ivy",
+    C: "Captain America"
+    D: "Superman"
+    },
+    correctAnswer: "D"
+    },
